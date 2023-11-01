0da25258d5b4fddde42bf848d6c14fa3e93fed7c
moabukar <mo-abukar10@hotmail.com>
2023-11-01 23:52:41 +0000

    fix resource type module

---
+++ examples/basic_resource_type/main.tf
@@ -0,0 +1,23 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+# Only protecting CloudFront Distributions
+
+module "shield_resource_type" {
+  source  = "../../modules/group_resource_type"
+  # version = "0.0.1"
+
+  tags = {
+    environment = "staging"
+  }
+  application_load_balancers = false
+  route53_hosted_zones       = false
+  global_accelerators        = false
+  classic_load_balancers     = false
+  elastic_ip_addresses       = false
+}
+
+output "shield_resource_type" {
+  value = module.shield_resource_type
+}
