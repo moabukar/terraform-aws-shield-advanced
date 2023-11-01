c66b78a58f38a135476edfb671ab5242dd4c6ec1
moabukar <mo-abukar10@hotmail.com>
2023-11-01 21:05:34 +0000

    update module example usage

--- examples/basic/main.tf
+++ examples/basic/main.tf
@@ -18,12 +18,11 @@ module "shield" {
   version = "0.0.1" 
   name = "example_shield"
 
-  # Pass in the name you wish to use for the resource, and the ARN of the resource to be protected.
   name_resource_arn_map = {
     "example_resource" = "arn:aws:ec2:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:eip-allocation/${aws_eip.example.id}"
   }
   tags = {
-    example = "true"
+    environment = "staging"
   }
 }
 
